9850c5a354ffa39719592c9ea24de824602d7b59
Pedro Almeida <ip4dro@gmail.com>
2024-04-01 12:07:02 -0300

    typo

--- PropertyInspector.swift
+++ PropertyInspector.swift
@@ -169,7 +169,7 @@ public struct PropertyInspectorShowcaseStyle: PropertyInspectorStyle {
                 configuration.header()
                     .padding(.horizontal)
             }
-        }   
+        }
     }
 }
 
